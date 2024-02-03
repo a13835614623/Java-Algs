63eb96a9a3cf7a90f1c868c4484c8b6280f0ddc9
zhangzikuan <zikuan.zhang@shopcider.com>
2024-02-03 20:40:00 +0800

    重构3:测试9

--- tdd/src/main/java/com/zzk/PrimeCalculator.java
+++ tdd/src/main/java/com/zzk/PrimeCalculator.java
@@ -7,22 +7,24 @@ import java.util.List;
 public class PrimeCalculator {
     public static List<Integer> calc(int input) {
         List<Integer> result = new ArrayList<>();
+        if (input == 1) {
+            return result;
+        }
         for (int i = 2; i <= input; i++) {
-            if (input % i == 0 && input > i) {
-                int rest = input;
-                result.add(i);
-                rest = rest / i;
-                while (rest % i == 0 && rest > i) {
-                    result.add(rest / i);
-                    rest /= i;
-                }
-                result.add(rest);
-                return result;
+            int rest = input;
+            if (rest % i != 0 || rest <= i) {
+                continue;
             }
+            result.add(i);
+            rest = rest / i;
+            while (rest % i == 0 && rest > i) {
+                result.add(rest / i);
+                rest /= i;
+            }
+            result.add(rest);
+            return result;
         }
-        if (input > 1) {
-            result.add(input);
-        }
+        result.add(input);
         return result;
     }
 }
