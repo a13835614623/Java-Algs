5afd58a036865a0db7b0392f389807259530b75e
zhangzikuan <zikuan.zhang@shopcider.com>
2024-02-03 16:02:57 +0800

    绿：测试3

--- tdd/src/main/java/com/zzk/PrimeCalculator.java
+++ tdd/src/main/java/com/zzk/PrimeCalculator.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class PrimeCalculator {
     public static List<Integer> calc(int input) {
-        if (input == 2) {
+        if (input == 2 || input == 3) {
             return List.of(input);
         }
         return List.of();
