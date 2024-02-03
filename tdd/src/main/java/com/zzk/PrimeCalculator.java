ebb0bec96ec0b7f46fb1e02d5fb714dd88a0080a
zhangzikuan <zikuan.zhang@shopcider.com>
2024-02-03 19:57:55 +0800

    重构2：测试8

--- tdd/src/main/java/com/zzk/PrimeCalculator.java
+++ tdd/src/main/java/com/zzk/PrimeCalculator.java
@@ -1,21 +1,24 @@
 package com.zzk;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeCalculator {
     public static List<Integer> calc(int input) {
+        List<Integer> result = new ArrayList<>();
         if (input % 2 == 0 && input > 2) {
-            if (input == 8) {
-                return List.of(2, 2, 2);
+            int rest = input;
+            result.add(2);
+            rest = rest / 2;
+            while (rest % 2 == 0 && rest > 2) {
+                result.add(rest / 2);
+                rest /= 2;
             }
-            return List.of(2, input / 2);
+            result.add(rest);
+        }else if (input>1){
+            result.add(input);
         }
-
-        if (input > 1) {
-            return List.of(input);
-        }
-
-        return List.of();
+        return result;
     }
 }
