20d4b2edca71e52169f0086818e168df369e81a0
zhangzikuan <zikuan.zhang@shopcider.com>
2024-02-03 16:00:40 +0800

    绿：测试2

--- tdd/src/main/java/com/zzk/PrimeCalculator.java
+++ tdd/src/main/java/com/zzk/PrimeCalculator.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class PrimeCalculator {
     public static List<Integer> calc(int i) {
+        if (i==2){
+            return List.of(2);
+        }
         return List.of();
     }
 }
